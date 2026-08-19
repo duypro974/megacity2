6db37676b43c53258210476b8a728e2edf9435f3
duypro974 <duypro974@gmail.com>
2026-08-19 18:09:32 +0700

    update map

--- src/app/mega-city-2/vi-tri/page.tsx
+++ src/app/mega-city-2/vi-tri/page.tsx
@@ -254,7 +254,7 @@ export default function ViTriPage() {
               <div className="rounded-2xl overflow-hidden border border-slate-200 shadow-sm anim-right anim-delay-100">
                 <iframe
                   title="Vị trí Mega City 2 Nhơn Trạch trên bản đồ"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3922.8!2d106.9667!3d10.7567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDQ1JzI0LjEiTiAxMDbCsDU4JzAwLjEiRQ!5e0!3m2!1svi!2svn!4v1"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31362.776564104588!2d106.8598273743164!3d10.707690599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317523006254ef57%3A0xc66fe772724b7d3!2sMega%20City%202%20-%20Kim%20Oanh%20Land!5e0!3m2!1svi!2s!4v1787137712216!5m2!1svi!2s"
                   width="100%"
                   height="420"
                   style={{ border: 0, display: "block" }}
